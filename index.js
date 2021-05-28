f45efd63238b582dd1c95ffebc0719734eccee9b
Robert George <rgeorge@midnightweb.net>
2021-05-28 16:14:07 -0700

    Fix lost idle connection and auto reconnect

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const ElectronPreferences = require('electron-preferences');
 const WebSocket = require('ws')
 const path = require('path');
 var _ws = null
+var _win = null
+
 app.userAgentFallback = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0'
 var ignored = []
 var campaignChars = []
@@ -30,6 +32,7 @@ preferences.on('save', (preferences) => {
 })
 app.on('ready', () => {
 	const win = new BrowserWindow({ width: 800, height: 600, webPreferences: {nodeIntegration: true, contextIsolation: false,nativeWindowOpen: true} })
+	_win = win
 	  var menu = Menu.buildFromTemplate([
 	      {
 		  label: 'File',
@@ -68,13 +71,22 @@ app.on('ready', () => {
 				}
 				[
 				document.getElementById('message-broker-client').dataset.gameid,
-				document.getElementById('message-broker-client').dataset.userid
+				document.getElementById('message-broker-client').dataset.userid,
+				document.getElementsByClassName('page-title')[0].textContent.trim()
 				];`)
-			.then(result => connectGameLog(result[0],result[1]))
+			.then(result => connectGameLog(result[0],result[1],result[2]))
 			.catch(err => console.log(err))
 		} else if (_ws !== null) {
-			_ws.terminate()
+			_ws.isDisconnecting = true
+			_ws.close(1001,"Going away")
 		}
+	})
+	win.on('beforeunload',() => {
+		if (_ws !== null) {
+			_ws.isDisconnecting = true
+			_ws.close(1001,"Going away")
+		}
+		win.loadURL('https://www.dndbeyond.com/my-campaigns')
 	})
 	ipcMain.on("changefilter", (event,data) => {
 		let name = data[0];
@@ -135,14 +147,19 @@ function requestCampaignChars(gameId,cobalt) {
 		    body += chunk.toString()
 		  })
 		  response.on('end', () => {
-	            campaignChars = JSON.parse(body).data
-		    resolve("ok")
+			try {
+	          	  campaignChars = JSON.parse(body).data
+			} catch (e) {
+			  console.log(e.message)
+			  console.log(body)
+			}
+		    	resolve("ok")
 		  })
 		})
 		request.end()
 	})
 }
-async function connectGameLog(gameId,userId) {
+async function connectGameLog(gameId,userId,campaignName) {
 	const cobalt = await getCobaltAuth()
 	requestCampaignChars(gameId,cobalt)
 	var url = new URL("wss://game-log-api-live.dndbeyond.com/v1")
@@ -150,12 +167,52 @@ async function connectGameLog(gameId,userId) {
 	url.searchParams.append('userId',userId)
 	url.searchParams.append('stt',cobalt)
 	if (_ws !== null) {
-		_ws.terminate()
+		_ws.isDisconnecting = true
+		_ws.close(1001,"Going away")
 	}
 	const ws = new WebSocket(url.toString())
-	ws.on('open',() => _ws = ws)
-	ws.on('close',() => _ws = null)
-	ws.on('error',(e) => _ws = null)
+	ws.on('open',() => {
+		_ws = ws
+		_ws.isDisconnecting = false
+		_ws.pingInterval = setInterval(() => _ws.ping(), 5000);
+		_win.webContents.executeJavaScript(`
+		if (document.getElementsByClassName('gamelog-button')[0]) {
+			document.getElementsByClassName('gamelog-button')[0].style.backgroundColor = 'LimeGreen';
+		}
+		`,true).catch((e) => console.log(e))
+		let msgJson = {
+		    "source": "EncounterLog",
+		    "type":     "message",
+		    "content": "Connected to D&D Beyond GameLog for " + campaignName.trim()
+		};
+		const request = net.request({url: encounterhost+"/api/messages",method: "POST"})
+		request.on('error',e => console.error(e))
+		request.write(JSON.stringify(msgJson))
+		request.end()
+	})
+	ws.on('close',(code,reason) => {
+		console.log(`WebSocket closed: ${reason} (${code})`)
+		_win.webContents.executeJavaScript(`
+		if (document.getElementsByClassName('gamelog-button')[0]) {
+			document.getElementsByClassName('gamelog-button')[0].style.backgroundColor = 'Crimson';
+		}
+		`,true).catch((e) => console.log(e))
+		clearInterval(_ws.pingInterval);
+		if (code == 1001 && !_ws.isDisconnecting) {
+			setTimeout(() => connectGameLog(gameId,userId,campaignName),1000)
+		}
+		_ws = null
+		let msgJson = {
+		    "source": "EncounterLog",
+		    "type":     "message",
+		    "content": "Disconnected from D&D Beyond GameLog for " + campaignName.trim()
+		};
+		const request = net.request({url: encounterhost+"/api/messages",method: "POST"})
+		request.on('error',e => console.error(e))
+		request.write(JSON.stringify(msgJson))
+		request.end()
+	})
+	ws.on('error',(e) => console.log(e))
 	ws.on('message',(data) => {
 		const msgData = JSON.parse(data)
 		var character = msgData.data.context.name?.trim() || ""
